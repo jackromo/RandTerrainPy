c9c21af3824ad666be0c75be3fd9691cac93626d
Jack Romo <sharrackor@gmail.com>
2016-03-27 17:29:18 +0200

    Add getter and setter methods to Terrain

--- randterrainpy/terrain.py
+++ randterrainpy/terrain.py
@@ -21,4 +21,26 @@ class Terrain(object):
         """
         self.width = width
         self.length = length
-        self.height_map = [0 for _ in self.width] * self.length
+        self.height_map = [[0 for _ in self.width]] * self.length
+
+    def __getitem__(self, item):
+        """Get an item at x-y coordinates.
+
+        Args:
+            item (tuple): 2-tuple of x and y coordinates.
+
+        Returns:
+            float: Height of terrain at coordinates, between 0 and 1.
+
+        """
+        return self.height_map[item[1]][item[0]]
+
+    def __setitem__(self, key, value):
+        """Set the height of an item.
+
+        Args:
+            key (tuple): 2-tuple of x and y coordinates.
+            value (float): New height of map at x and y coordinates, between 0 and 1.
+
+        """
+        self.height_map[key[1]][key[0]] = value
