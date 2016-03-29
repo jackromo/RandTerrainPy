8f8b313a1b5118b6528e5152252128e075de0401
Jack Romo <sharrackor@gmail.com>
2016-03-29 02:11:03 +0200

    Add tests for indexing, equality and addition for Terrain

--- tests/test_terrain.py
+++ tests/test_terrain.py
@@ -5,4 +5,28 @@ from randterrainpy import *
 class TerrainTesterPy(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.ter1 = Terrain(1, 1)
+        self.ter2 = Terrain(2, 4)
+        self.ter3 = Terrain(1, 1)
+
+    def test_getitem(self):
+        self.assertEqual(self.ter1[0, 0], 0)
+        self.assertEqual(self.ter2[1, 2], 0)
+
+    def test_eq(self):
+        self.assertEqual(self.ter1, self.ter3)
+        self.assertNotEqual(self.ter1, self.ter2)
+
+    def test_setitem(self):
+        self.ter1[0, 0] = 1
+        self.assertEqual(self.ter1[0, 0], 1)
+        self.ter2[1, 2] = 0.5
+        self.assertEqual(self.ter2[1, 2], 0.5)
+
+    def test_add(self):
+        self.assertRaises(InvalidDimensionsError, self.ter1.__add__, self.ter2)
+        self.assertEqual(self.ter1+self.ter3, self.ter1)
+
+
+if __name__ == "__main__":
+    unittest.main()
