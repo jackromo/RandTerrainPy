4b3e1a1fa2dadcccd65d974a27c72169a68c6a61
Jack Romo <sharrackor@gmail.com>
2016-03-29 02:15:51 +0200

    Add subtraction and scalar multiplication tests for Terrain

--- tests/test_terrain.py
+++ tests/test_terrain.py
@@ -18,14 +18,29 @@ class TerrainTesterPy(unittest.TestCase):
         self.assertNotEqual(self.ter1, self.ter2)
 
     def test_setitem(self):
-        self.ter1[0, 0] = 1
-        self.assertEqual(self.ter1[0, 0], 1)
+        self.ter1[0, 0] = 0.9
+        self.assertEqual(self.ter1[0, 0], 0.9)
         self.ter2[1, 2] = 0.5
         self.assertEqual(self.ter2[1, 2], 0.5)
 
     def test_add(self):
         self.assertRaises(InvalidDimensionsError, self.ter1.__add__, self.ter2)
         self.assertEqual(self.ter1+self.ter3, self.ter1)
+        test_ter = Terrain(1, 1)
+        test_ter[0, 0] = 1
+        self.assertEqual((self.ter1+test_ter)[0, 0], 1)     # capped at 1
+
+    def test_sub(self):
+        self.assertRaises(InvalidDimensionsError, self.ter1.__sub__, self.ter2)
+        self.assertEqual(self.ter1-self.ter3, self.ter1)
+        test_ter = Terrain(1, 1)
+        test_ter[0, 0] = 1
+        self.assertEqual((self.ter1-test_ter)[0, 0], 0)     # capped at 0
+
+    def test_mul(self):
+        self.assertEqual(self.ter1*0, Terrain(self.ter1.width, self.ter1.length))
+        self.assertEqual(self.ter2*1, self.ter2)
+        self.assertNotEqual(self.ter2*0.5, self.ter2)
 
 
 if __name__ == "__main__":
